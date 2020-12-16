965ab43c7c16dc7b5e2eb99578c092bec4439478
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-12-16 11:31:24 -0500

    syntax for objects and arrays in ts

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -5,3 +5,16 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
