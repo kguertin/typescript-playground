d85189df614a4d7f9df84f4555e5e6459bf7d963
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-12-19 14:09:56 -0500

    access object keys and set type annotations with function ts

--- features/tuples.ts
+++ features/tuples.ts
@@ -11,3 +11,6 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 45];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354]; // Might use with CVS files
+const carStats = { horsepower: 400, weight: 3354 }; // This is a more clear syntax
