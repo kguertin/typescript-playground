98ecffba8de4fe64702fa1156f29b00e21a60b52
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-12-19 12:36:00 -0500

    benifits of adding types to arrays

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -2,3 +2,15 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values from being added to array
+carMakers.push(100);
+
+// Help with map, foreach, etc.
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
