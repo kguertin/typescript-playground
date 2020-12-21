bbc416d3ff76057cb658156a2b64a9ba443d1619
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-12-21 11:09:48 -0500

    class inheritance and modifers in ts

--- features/classes.ts
+++ features/classes.ts
@@ -1,13 +1,20 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-
-  honk(): void {
+  protected honk(): void {
     console.log('Beep');
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+class Car extends Vehicle {
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car();
+car.startDrivingProcess();
+car.honk();
