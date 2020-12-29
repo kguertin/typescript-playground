8af59d91fe8b233b294142380353e4f33162f605
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-12-29 16:01:30 -0500

    add method to user and company classes that meets the mappable interface requirements and genereates info window content

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -100437,6 +100437,10 @@ function () {
     };
   }
 
+  User.prototype.markerContent = function () {
+    return "\n    <div>\n      <h1>User Name: " + this.name + "</h1>\n    </div>\n    ";
+  };
+
   return User;
 }();
 
@@ -100469,6 +100473,10 @@ function () {
     };
   }
 
+  Company.prototype.markerContent = function () {
+    return "\n    <div>\n      <h1>Company Name: " + this.companyName + "</h1>\n      <h3>Catch Phrase: " + this.catchPhrase + "</h3>\n    </div>\n    ";
+  };
+
   return Company;
 }();
 
@@ -100495,12 +100503,20 @@ function () {
   }
 
   CustomMap.prototype.addMarker = function (mappable) {
-    new google.maps.Marker({
+    var _this = this;
+
+    var marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+    marker.addListener('click', function () {
+      var infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+      infoWindow.open(_this.googleMap, marker);
     });
   };
 
@@ -100554,7 +100570,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58350" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58502" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -6,11 +6,20 @@ export class User {
     lat: number;
     lng: number;
   };
+
   constructor() {
     this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h1>User Name: ${this.name}</h1>
+    </div>
+    `;
   }
 }
