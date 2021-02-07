43269958ed4cc3c21008936cbd4ad3056a0bb13a
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-02-06 22:47:11 -0500

    create user model

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hi There');
+import { User } from './models/user';
+
+const user = new User({ name: 'myName', age: 30 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
