97e5c0f589057508ea10a2300477659c2e270efb
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-02-08 11:12:07 -0500

    can now trigger events

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,8 +2,14 @@ import { User } from './models/user';
 
 const user = new User({ name: 'myName', age: 30 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('stop', () => {});
+user.on('change', () => {
+  console.log('change 1');
+});
+user.on('change', () => {
+  console.log('change 2');
+});
+user.on('stop', () => {
+  console.log('STOP');
+});
 
-console.log(user);
+user.trigger('stop');
