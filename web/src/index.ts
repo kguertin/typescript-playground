0750a997ef85c0bd93ac7cd191bb31159d51662b
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-02-07 22:05:10 -0500

    add optional keys to interface and create set method

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,9 @@ import { User } from './models/user';
 
 const user = new User({ name: 'myName', age: 30 });
 
+user.set({ name: 'newName', age: 999 });
+
+user.set({ name: 'anotherName' });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
