b7f520cd147015b7951ebf9cabc29e4e5ff742b6
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-02-27 19:39:23 -0500

    add eventing to user model

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,3 +3,7 @@ import { User } from './models/User';
 const user = new User({ name: 'NEW_RECORD', age: 0 });
 
 user.save();
+user.events.on('click', () => console.log('THIS IS A CLICK'));
+user.events.on('click', () => console.log('THIS IS ANOTHER CLICK'));
+
+user.events.trigger('click');
