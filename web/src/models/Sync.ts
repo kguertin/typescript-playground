72c858e8293f99e7c2559504a45c48f94844c9dc
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-02-28 14:16:54 -0500

    make sync generic class

--- web/src/models/Sync.ts
+++ web/src/models/Sync.ts
@@ -1,14 +1,17 @@
 import axios, { AxiosPromise } from 'axios';
-import { UserProps } from './User';
 
-export class Sync {
+interface hasId {
+  id: number;
+}
+
+export class Sync<T extends hasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
 
     if (id) {
