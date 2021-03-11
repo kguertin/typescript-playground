077c07adcd577743145e758b5417125176b4ae4a
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-03-11 16:20:57 -0500

    fix error with pass through methods

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,6 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'NEW_RECORD', age: 0 });
 
+console.log(user.get('name'));
+
 user.on('change', () => {
   console.log('user was changed');
 });
+
+user.trigger('change');
