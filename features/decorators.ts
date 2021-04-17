81886b65b482f853fa0e0c67ef465dcf140f2693
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-04-17 18:01:42 -0400

    create class and parameter decorator

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,15 +1,37 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'blue';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
   @logError('Oops, boat was sunk in ocean.')
-  pilot(): void {
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
     throw new Error();
-    console.log('swish');
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
 }
 
 function logError(errorMessage: string) {
@@ -24,5 +46,3 @@ function logError(errorMessage: string) {
     };
   };
 }
-
-new Boat().pilot();
