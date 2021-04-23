f10a2c398e39d5167eb9c1e9cded76998ecfec47
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-04-23 09:30:49 -0400

    use meta data in decorator function

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,17 +1,35 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
+// const plane = {
+//   color: 'red',
+// };
 
-Reflect.defineMetadata('note', 'hi there', plane);
-Reflect.defineMetadata('height', 10, plane);
+// Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
 
-const note = Reflect.getMetadata('note', plane);
-const height = Reflect.getMetadata('height', plane);
-console.log(note);
-console.log(height);
+// const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
+// console.log(note);
+// console.log(height);
 
-Reflect.defineMetadata('test', 'hey there', plane, 'color');
-const test = Reflect.getMetadata('test', plane, 'color');
-console.log(test);
+// Reflect.defineMetadata('test', 'hey there', plane, 'color');
+// const test = Reflect.getMetadata('test', plane, 'color');
+// console.log(test);
+
+class Plane {
+  color: string = 'red';
+
+  @markFunction('Hi There')
+  fly(): void {
+    console.log('Vrrrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+console.log(secret);
