db1c70b261e94df89d54345a6f486a8c8c6e4c23
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-11-03 13:07:23 -0400

    create react app to test wrapping material ui components

---
+++ material-ui/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './App.css';
+
+import Home from './ui/home';
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ material-ui/src/views/home.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <p>Test Home</p>
+    </div>
+  );
+}
+
+export default Home;
